c5047e512a6256f69f6d344d277e2bfa38641e11
CassieV <obicassievirtue@gmail.com>
2022-07-25 17:18:10 +0200

    Use method to create and display game board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,15 +1,60 @@
 
 
-class TicTacToe
 
-  def create_board()
-    puts "__|__|__"
-    puts "__|__|__"
+class TicTacToe
+  
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+  
+  def self.create_board()
     puts "  |  |  "
+    puts "__ __ __"
+    puts "  |  |  "
+    puts "__ __ __"
+    puts "  |  |  "
+    puts "Player 1 - o and Player 2 - x"
   end
 
+  def self.play_game
+    
+
+  end
+    
+
+  end
+
+  
 
 end
 
 TicTacToe.create_board()
+TicTacToe.play_game()
+
+
+
+# def self.play_game
+#   @player1 = gets
+#   if @player1 == "1"
+#     puts " o|  |  "
+#     puts "__ __ __"
+#     puts "  |  |  "
+#     puts "__ __ __"
+#     puts "  |  |  "
+#   elsif  @player1 == "2"
+#     puts "  | o|  "
+#     puts "__ __ __"
+#     puts "  |  |  "
+#     puts "__ __ __"
+#     puts "  |  |  "
+#   elsif  @player1 == "3"
+#     puts "  |  | o"
+#     puts "__ __ __"
+#     puts "  |  |  "
+#     puts "__ __ __"
+#     puts "  |  |  "
+#   end
+  
 
