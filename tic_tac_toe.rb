213a1fbd0730dab36d413f2855f3f47bd9398b61
CassieV <obicassievirtue@gmail.com>
2022-07-25 19:18:35 +0200

    Create method to place o or x in game board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,24 +4,46 @@
 class TicTacToe
   
 
-  def initialize(player1, player2)
-    @player1 = player1
-    @player2 = player2
-  end
+  # def initialize(player1, player2)
+  #   @player1 = player1
+  #   @player2 = player2
+  # end
+  @@grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   
   def self.create_board()
-    puts "  |  |  "
-    puts "__ __ __"
-    puts "  |  |  "
-    puts "__ __ __"
-    puts "  |  |  "
+    
+    puts " #{@@grid[0]} | #{@@grid[1]} | #{@@grid[2]} "
+    puts "___________"
+    puts " #{@@grid[3]} | #{@@grid[4]} | #{@@grid[5]} "
+    puts "___________"
+    puts " #{@@grid[6]} | #{@@grid[7]} | #{@@grid[8]} "
     puts "Player 1 - o and Player 2 - x"
   end
 
   def self.play_game
     
-
-  end
+    player1 = gets.chomp
+    grid = @@grid.collect do |value|
+      if value == player1.to_i
+        value = "o"
+      else
+        value
+      end
+    end
+    p grid
+    @@grid = grid
+    puts self.create_board
+    player2 = gets.chomp
+    grid = @@grid.collect do |value|
+      if value == player2.to_i
+        value = "x"
+      else
+        value
+      end
+    end
+    p grid
+    @@grid = grid
+    puts self.create_board
     
 
   end
