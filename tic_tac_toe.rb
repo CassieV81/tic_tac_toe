17bdd99151668cf83e7c590e96aaf79e2278ab67
CassieV <obicassievirtue@gmail.com>
2022-07-28 17:27:28 +0200

    Reiterate play game method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class TicTacToe
-
-
   def initialize
     @grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
@@ -13,11 +11,11 @@ class TicTacToe
     puts " #{@grid[3]} | #{@grid[4]} | #{@grid[5]} "
     puts '___________'
     puts " #{@grid[6]} | #{@grid[7]} | #{@grid[8]} "
-    puts 'Player 1 - o and Player 2 - x'
+    puts 'Player 1 = o and Player 2 = x'
   end
 
   def player1_selection
-    print 'Player 1: '
+    print 'Player 1: make your selection '
     player1 = gets.chomp.to_i
     grid = @grid.collect do |value|
       value == player1 ? 'o' : value
@@ -27,7 +25,7 @@ class TicTacToe
   end
 
   def player2_selection
-    print 'Player 2: '
+    print 'Player 2: make your selection '
     player2 = gets.chomp.to_i
     grid = @grid.collect do |value|
       value == player2 ? 'x' : value
@@ -36,48 +34,34 @@ class TicTacToe
     puts create_board
   end
 
-  # def player1_win_combo?
+  def player1_win?
+    @grid[0] == 'o' && @grid[1] == 'o' && @grid[2] == 'o' ||
+      @grid[3] == 'o' && @grid[4] == 'o' && @grid[5] == 'o' ||
+      @grid[6] == 'o' && @grid[7] == 'o' && @grid[8] == 'o' ||
+      @grid[0] == 'o' && @grid[3] == 'o' && @grid[6] == 'o' ||
+      @grid[1] == 'o' && @grid[4] == 'o' && @grid[7] == 'o' ||
+      @grid[2] == 'o' && @grid[5] == 'o' && @grid[8] == 'o' ||
+      @grid[0] == 'o' && @grid[4] == 'o' && @grid[8] == 'o' ||
+      @grid[2] == 'o' && @grid[4] == 'o' && @grid[6] == 'o'
+  end
 
-  #   @grid[0] == "o" && @grid[1] == "o" && @grid[2] == "o"
-  #   @grid[3] == "o" && @grid[4] == "o" && @grid[5] == "o"
-  #   @grid[6] == "o" && @grid[7] == "o" && @grid[8] == "o"
-  #   @grid[0] == "o" && @grid[3] == "o" && @grid[6] == "o"
-  #   @grid[1] == "o" && @grid[4] == "o" && @grid[7] == "o"
-  #   @grid[2] == "o" && @grid[5] == "o" && @grid[8] == "o"
-  #   @grid[0] == "o" && @grid[4] == "o" && @grid[8] == "o"
-  #   @grid[2] == "o" && @grid[4] == "o" && @grid[6] == "o"
-
-  # end
-
-  def check_winner
-    if @grid[0] == 'o' && @grid[1] == 'o' && @grid[2] == 'o' ||
-       @grid[3] == 'o' && @grid[4] == 'o' && @grid[5] == 'o' ||
-       @grid[6] == 'o' && @grid[7] == 'o' && @grid[8] == 'o' ||
-       @grid[0] == 'o' && @grid[3] == 'o' && @grid[6] == 'o' ||
-       @grid[1] == 'o' && @grid[4] == 'o' && @grid[7] == 'o' ||
-       @grid[2] == 'o' && @grid[5] == 'o' && @grid[8] == 'o' ||
-       @grid[0] == 'o' && @grid[4] == 'o' && @grid[8] == 'o' ||
-       @grid[2] == 'o' && @grid[4] == 'o' && @grid[6] == 'o'
-      puts 'Player 1 wins'
-    elsif @grid[0] == 'x' && @grid[1] == 'x' && @grid[2] == 'x' ||
-          @grid[3] == 'x' && @grid[4] == 'x' && @grid[5] == 'x' ||
-          @grid[6] == 'x' && @grid[7] == 'x' && @grid[8] == 'x' ||
-          @grid[0] == 'x' && @grid[3] == 'x' && @grid[6] == 'x' ||
-          @grid[1] == 'x' && @grid[4] == 'x' && @grid[7] == 'x' ||
-          @grid[2] == 'x' && @grid[5] == 'x' && @grid[8] == 'x' ||
-          @grid[0] == 'x' && @grid[4] == 'x' && @grid[8] == 'x' ||
-          @grid[2] == 'x' && @grid[4] == 'x' && @grid[6] == 'x'
-      puts 'Player 2 wins'
-      nil
-    end
+  def player2_win?
+    @grid[0] == 'x' && @grid[1] == 'x' && @grid[2] == 'x' ||
+      @grid[3] == 'x' && @grid[4] == 'x' && @grid[5] == 'x' ||
+      @grid[6] == 'x' && @grid[7] == 'x' && @grid[8] == 'x' ||
+      @grid[0] == 'x' && @grid[3] == 'x' && @grid[6] == 'x' ||
+      @grid[1] == 'x' && @grid[4] == 'x' && @grid[7] == 'x' ||
+      @grid[2] == 'x' && @grid[5] == 'x' && @grid[8] == 'x' ||
+      @grid[0] == 'x' && @grid[4] == 'x' && @grid[8] == 'x' ||
+      @grid[2] == 'x' && @grid[4] == 'x' && @grid[6] == 'x'
   end
 
   def play_game
-    loop do
+    until player1_win? == true || player2_win? == true
       player1_selection
-      return if check_winner == true
-
+      puts 'Player 1 wins' if player1_win? == true
       player2_selection
+      puts 'Player 2 wins' if player2_win? == true
     end
   end
 end
